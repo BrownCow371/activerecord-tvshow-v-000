36d94fd17934dc2dad962829bc7904efce5c52f8
Jamie Brown <jmbrowncow@gmail.com>
2018-07-29 00:40:27 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,21 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+
+  def most_popular_show
+    self.highest_rating
+  end
+
+  def lowest_rating
+  end
+
+  def least_popular_show
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
+  
 end
